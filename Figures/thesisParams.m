c1601cfcc9b6ecd7bb0e5e44a95200ea75319322
Hayden Scott <43681457+Hscott5@users.noreply.github.com>
2023-06-21 11:25:58 -0400

    continued work

--- Figures/thesisParams.m
+++ Figures/thesisParams.m
@@ -2,11 +2,25 @@ function params = thesisParams
 
 
 % list of colors to pull from for consistency
-params.royalBlue   = [16, 66, 122]./255; % Title 'RoyalBlue'
-params.mahogany    = [180, 50, 30]./255; % Mahogany
+% Red
+params.ddi         = [255, 170, 090]./255;
+params.mahogany    = [180, 050, 030]./255; 
+% Blue
+params.active      = [123, 050, 148]./255;
+params.lavender    = [190, 186, 218]./255;
+params.royalBlue   = [016, 066, 122]./255; 
+params.midBlue     = [31, 120, 180]./255;
+params.idi         = [128, 177, 211]./255;
+params.lightBlue   = [166, 206, 227]./255;
+% Green
+params.passive     = [000, 136, 055]./255;
+params.lime = [178, 223, 138]./255;
 params.springGreen = [196, 220, 110]./255; 
+params.cyan = [0, 1, 1];
+params.magenta = [1,0,1];
+
+% Grays
 params.gray        = [155, 156, 159]./255; 
-% params.cyclic;
 
 
 alpha = linspace(1,0,100);
